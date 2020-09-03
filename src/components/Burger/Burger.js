f45bd0c839928eb0111212ad4285006dcffb1c56
Andrew Bratcher <abratcher1066@gmail.com>
2020-09-03 16:58:30 -0400

    Successful static burger; now structuring dynamic burger builder

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,27 +3,19 @@ import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
-  let transformedIngredients = Object.keys(props.ingredients)
-    .map(ingredient => {
-      return [...Array(props.ingredients[ingredient])].map((_, i) => {
-        return <BurgerIngredient key={ingredient + i} type={ingredient} />
-      });
-    })
-    .reduce((arr,el) => {
-      return arr.concat(el);
-    }, []);
-
-  if (transformedIngredients.length === 0) {
-    transformedIngredients =<p>Please start adding ingredients!</p>;
-  }
-
-  return (
-    <div className={classes.Burger}>
-      <BurgerIngredient type='bread-top' />
-      {transformedIngredients}
-      <BurgerIngredient type='bread-bottom' />
-    </div>
-  );
+    const transformedIngredients = Object.keys(props.ingredients)
+        .map(igKey => {
+            return [...Array(props.ingredients[igKey])]
+        });
+    return (
+        <div className={classes.Burger}>
+            <BurgerIngredient type="bread-top" />
+            <BurgerIngredient type="cheese" />
+            <BurgerIngredient type="meat" />
+            <BurgerIngredient type="bread-bottom" />
+        </div>
+    );
 };
+
 
 export default burger;

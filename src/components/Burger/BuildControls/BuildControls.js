7e025282acde6843da9eb2458dc27a6ae8bb8ebc
Andrew Bratcher <abratcher1066@gmail.com>
2020-09-03 17:34:24 -0400

    Added basic builder controls ui, not functional yet

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -1,30 +1,22 @@
 import React from 'react';
-import classes from './BuildControls.css';
+import classes from './BuildControls.css'
 import BuildControl from './BuildControl/BuildControl';
 
 const controls = [
-  { label: 'Salad', type: 'salad' },
-  { label: 'Bacon', type: 'bacon' },
-  { label: 'Cheese', type: 'cheese' },
-  { label: 'Meat', type: 'meat' }
+    { label: 'Salad', type: 'salad' },
+    { label: 'Bacon', type: 'bacon' },
+    { label: 'Cheese', type: 'cheese' },
+    { label: 'Meat', type: 'meat' },
 ];
 
+
 const buildControls = (props) => (
-  <div className={classes.BuildControls}>
-    <p>Current Price: <strong>${props.price.toFixed(2)}</strong></p>
-    {controls.map(ctrl => (
-      <BuildControl 
-        key={ctrl.label} 
-        label={ctrl.label} 
-        added={() => props.ingredientAdded(ctrl.type)}
-        removed={() => props.ingredientRemoved(ctrl.type)}
-        disable={props.disabled[ctrl.type]} />
-    ))}
-    <button 
-      className={classes.OrderButton}
-      disabled={!props.purchaseable}
-      onClick={props.ordered}>Order Now!</button>
-  </div>
+    <div className={classes.BuildControls}>
+        {controls.map(ctrl => (
+            <BuildControl key={ctrl.label} label={ctrl.label} />
+        ))}
+    </div>
+
 );
 
 export default buildControls;

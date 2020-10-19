f80c8de388d67e14486da02b234c96e505be9f3b
Andrew Bratcher <abratcher1066@gmail.com>
2020-10-19 11:15:28 -0400

    Add files via upload

--- deleteme/scripts/index.js
+++ deleteme/scripts/index.js
@@ -2,13 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import 'react-mdl/extra/material.css';
-import 'react-mdl/extra/material.js';
-import { BrowserRouter } from 'react-router-dom'
+import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(
-  <BrowserRouter>
-      <App />
-  </BrowserRouter>
-  , document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
